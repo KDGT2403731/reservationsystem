5cba9f038bf95de6d0e23c690a68354cd5af3389
KDGT2403731 <shibakawayuya@gmail.com>
2026-01-20 14:22:06 +0900

    2026-01-20 編集

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,14 +1,14 @@
 INSERT INTO users (name, email, password, role) VALUES
 	-- 顧客（ログイン ID: customer○@example.com / 仮パスワード: password）
-	('顧客 A', 'customerA@example.com', '$2a$10$slYQmyNdGzin7olVN3p5be4DlH.PKZbv5H8KnzzVgXXbVxzy5QFZO', 'CUSTOMER'),
-	('顧客 B', 'customerB@example.com', '$2a$10$slYQmyNdGzin7olVN3p5be4DlH.PKZbv5H8KnzzVgXXbVxzy5QFZO', 'CUSTOMER'),
-	('顧客 C', 'customerC@example.com', '$2a$10$slYQmyNdGzin7olVN3p5be4DlH.PKZbv5H8KnzzVgXXbVxzy5QFZO', 'CUSTOMER'),
+	('顧客 A', 'customerA@example.com', 'password', 'CUSTOMER'),
+	('顧客 B', 'customerB@example.com', 'password2', 'CUSTOMER'),
+	('顧客 C', 'customerC@example.com', 'password3', 'CUSTOMER'),
 	-- スタッフ（ログイン ID: staff○@example.com / 仮パスワード: staffpass）
-	('スタッフ A', 'staffA@example.com', '$2a$10$0p5u6ggT29TE1IKg8vkfReJNLxj1WKxVzCkW6Nw5DwhKV0DzXyYmC', 'STAFF'),
-	('スタッフ B', 'staffB@example.com', '$2a$10$0p5u6ggT29TE1IKg8vkfReJNLxj1WKxVzCkW6Nw5DwhKV0DzXyYmC', 'STAFF'),
-	('スタッフ C', 'staffC@example.com', '$2a$10$0p5u6ggT29TE1IKg8vkfReJNLxj1WKxVzCkW6Nw5DwhKV0DzXyYmC', 'STAFF'),
+	('スタッフ A', 'staffA@example.com', 'staffpass1', 'STAFF'),
+	('スタッフ B', 'staffB@example.com', 'staffpass2', 'STAFF'),
+	('スタッフ C', 'staffC@example.com', 'staffpass3', 'STAFF'),
 	-- 管理者（ログイン ID: admin○@example.com / 仮パスワード: adminpass）
-	('管理者 A', 'adminA@example.com', '$2a$10$FGLkTw8OHBxHlLVqNRk0TuQB1jNdKVjz2Zz9Q7s5J0Q0Y7fK1vJ4y', 'ADMIN');
+	('管理者 A', 'adminA@example.com', 'adminpass', 'ADMIN');
 	
 INSERT INTO shift (staff_id, record_date, start_time, end_time) VALUES
 	((SELECT id FROM users WHERE email = 'staffA@example.com'), CURRENT_DATE, '09:00:00', '17:00:00'),
