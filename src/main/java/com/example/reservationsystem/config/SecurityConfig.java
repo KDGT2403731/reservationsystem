e190d49117432f6c08d90e3fdac31846236deff1
KDGT2403731 <shibakawayuya@gmail.com>
2026-01-21 16:04:44 +0900

    2026-01-21 securityconfig変更

--- src/main/java/com/example/reservationsystem/config/SecurityConfig.java
+++ src/main/java/com/example/reservationsystem/config/SecurityConfig.java
@@ -40,7 +40,7 @@ public class SecurityConfig {
 				.map(user -> org.springframework.security.core.userdetails.User.builder()
 						.username(user.getEmail())
 						.password(user.getPassword())
-						.roles(user.getRole())
+						.roles(user.getRole().replace("ROLE_", ""))
 						.build())
 				.orElseThrow(() -> new UsernameNotFoundException("User not found: " + email));
 	}
